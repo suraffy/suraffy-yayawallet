a54bab8932f212deba82140e00078dfade41d9d1
Surafel Araya <surafelay@gmail.com>
2024-02-14 15:38:36 +0300

    tailwind.config: configure container

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      padding: "1rem",
+      center: true,
+    },
   },
   plugins: [],
 };
