e52ab6b1c8235e18137e4e66b4cc46f2a0cc5d4f
Surafel Araya <surafelay@gmail.com>
2024-02-15 09:57:09 +0300

    Dashboard: display transaction list

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Header from "./common/Header";
+import TanstackTable from "./common/TanstackTable";
+import { transactions, Transaction } from "../db";
+
+const columns = [
+  { accessorKey: "transactionID", header: "Transaction ID" },
+  { accessorKey: "sender", header: "Sender" },
+  { accessorKey: "receiver", header: "Receiver" },
+  { accessorKey: "currency", header: "Currency" },
+  { accessorKey: "cause", header: "Cause" },
+  { accessorKey: "createdAt", header: "Created At" },
+];
 
 const Dashboard = () => {
+  const [transactionList] = useState<Transaction[]>(transactions);
   return (
     <>
       <Header />
+      <div className="containerr px-5 mt-5">
+        <TanstackTable data={transactionList} columns={columns} />
+      </div>
     </>
   );
 };
