c861f040011ba8fa6e8245f55aca07b1b59686b9
Surafel Araya <surafelay@gmail.com>
2024-02-15 10:22:29 +0300

    TanstackTable: Implement pagination

--- src/components/common/TanstackTable.tsx
+++ src/components/common/TanstackTable.tsx
@@ -3,6 +3,7 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getPaginationRowModel,
 } from "@tanstack/react-table";
 import { Transaction } from "../../db";
 
@@ -16,44 +17,73 @@ const TanstackTable = ({ data, columns }: Props) => {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
-    <table className="table-auto border-collapse w-full">
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id} className="bg-blue-50">
-            {headerGroup.headers.map((header) => (
-              <th
-                key={header.id}
-                className="border-t border-b border-slate-100 text-left py-2 px-4"
-              >
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id} className="hover:bg-blue-50">
-            {row.getVisibleCells().map((cell) => (
-              <td
-                key={cell.id}
-                className="border-t border-b border-slate-200 py-2 px-4"
-              >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <table className="table-auto border-collapse w-full">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="bg-blue-50">
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  className="border-t border-b border-slate-100 text-left py-2 px-4"
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className="hover:bg-blue-50">
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className="border-t border-b border-slate-200 py-2 px-4"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="mt-5">
+        <button className="border px-3" onClick={() => table.setPageIndex(0)}>
+          First page
+        </button>
+        <button
+          disabled={!table.getCanPreviousPage()}
+          className="border px-3"
+          onClick={() => table.previousPage()}
+        >
+          {"<<"}
+        </button>
+        <button
+          disabled={!table.getCanNextPage()}
+          className="border px-3"
+          onClick={() => table.nextPage()}
+        >
+          {">>"}
+        </button>
+        <button
+          className="border px-3"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+        >
+          Last page
+        </button>
+      </div>
+    </>
   );
 };
 
