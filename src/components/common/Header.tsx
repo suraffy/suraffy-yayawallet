6318c85cf499d7f12bcc20878dbeefa8022a8dd9
Surafel Araya <surafelay@gmail.com>
2024-02-15 09:55:05 +0300

    Header: update header layout

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,22 +3,22 @@ import yayawalletLogo from "../../assets/yayawallet logo.svg";
 const Header = () => {
   return (
     <div className="shadow">
-      <header className="container flex items-center justify-between">
+      <header className="container flex justify-between">
         <img src={yayawalletLogo} width={200} alt="" />
 
-        <ul className="flex gap-x-4 text-lg">
-          <li>
-            <a href="" className="font-semibold text-blue-800">
+        <ul className="flex text-lg">
+          <li className="hover:bg-blue-50 flex items-center">
+            <a href="" className="font-semibold text-blue-800 p-4">
               Dashboard
             </a>
           </li>
-          <li>
-            <a href="" className="hover:text-blue-600">
+          <li className="hover:bg-blue-50 flex items-center">
+            <a href="" className="hover:text-blue-800 p-4">
               Help&nbsp;Center
             </a>
           </li>
-          <li>
-            <a href="" className="hover:text-blue-600">
+          <li className="hover:bg-blue-50 flex items-center">
+            <a href="" className="hover:text-blue-800 p-4">
               Login
             </a>
           </li>
