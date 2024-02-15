09517d5cba607d970db9ec6ae0ea0747c67af5bc
Surafel Araya <surafelay@gmail.com>
2024-02-15 09:54:41 +0300

    Create a local database

---
+++ src/db.ts
@@ -0,0 +1,139 @@
+export type Transaction = {
+  transactionID: string;
+  sender: string;
+  receiver: string;
+  currency: string;
+  cause: string;
+  createdAt: string;
+};
+
+export const transactions = [
+  {
+    transactionID: "01",
+    sender: "John Doe",
+    receiver: "Porsche Camden",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "02",
+    sender: "Thom Bunny",
+    receiver: "Tyler Autumn",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "03",
+    sender: "Netta Lavern",
+    receiver: "Pepper Windsor",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "04",
+    sender: "Bob Johnson",
+    receiver: "Emily Davis",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "05",
+    sender: "William Brown",
+    receiver: "Laura Wilson",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "06",
+    sender: "Michael Garcia",
+    receiver: "Stephanie Lee",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "07",
+    sender: "David Hernandez",
+    receiver: "Jessica Perez",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "08",
+    sender: "Mark Thompson",
+    receiver: "Lisa Rodriguez",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "Julia Kim",
+    receiver: "Max Brown",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "Oliver Martinez",
+    receiver: "Sophie Lee",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "Max Brown",
+    receiver: "Mia Nguyen",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "Lucas Kim",
+    receiver: "Isabe Hernan",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "Ethan Kim",
+    receiver: "Ava Patel",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "William Kim",
+    receiver: "Ella Lee",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "Noah Martin",
+    receiver: "Emma Garcia",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    transactionID: "01",
+    sender: "Aiden Martinez",
+    receiver: "Chloe Nguyen",
+    currency: "ETB",
+    cause: "No reason",
+    createdAt: new Date().toISOString(),
+  },
+];
