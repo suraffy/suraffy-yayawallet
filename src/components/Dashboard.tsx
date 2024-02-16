e9d39c5a9da7138d6c629314c2c7b947bb1c1b25
Surafel Araya <surafelay@gmail.com>
2024-02-16 10:11:45 +0300

    Dashboard: access query client and render transactions

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,24 +1,44 @@
-import { useState } from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
 import Header from "./common/Header";
 import TanstackTable from "./common/TanstackTable";
-import { transactions, Transaction } from "../db";
 
 const columns = [
   { accessorKey: "transactionID", header: "Transaction ID" },
   { accessorKey: "sender", header: "Sender" },
   { accessorKey: "receiver", header: "Receiver" },
+  { accessorKey: "amount", header: "Amount" },
   { accessorKey: "currency", header: "Currency" },
   { accessorKey: "cause", header: "Cause" },
   { accessorKey: "createdAt", header: "Created At" },
 ];
 
 const Dashboard = () => {
-  const [transactionList] = useState<Transaction[]>(transactions);
+  const apiEndpoint = "https://sura-yaya-api.onrender.com/transactions";
+  const { data, isLoading } = useQuery("suraU", () => axios.get(apiEndpoint));
+
+  const transactions = data?.data.data.map((t) => ({
+    transactionID: t.id,
+    sender: t.sender.name,
+    receiver: t.receiver.name,
+    amount: t.amount,
+    currency: t.currency,
+    cause: t.cause || "-",
+    createdAt: t.created_at_time,
+  }));
+
+  console.log(transactions);
+
   return (
     <>
       <Header />
       <div className="containerr px-5 mt-5">
-        <TanstackTable data={transactionList} columns={columns} />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <TanstackTable data={transactions} columns={columns} />
+        )}
       </div>
     </>
   );
