43adc8bbf3db6cbab6bb117da00550b88fa57e5d
Surafel Araya <surafelay@gmail.com>
2024-02-16 09:46:25 +0300

    App: provide react-query client to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import Dashboard from "./components/Dashboard";
 
-function App() {
+const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Dashboard />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
