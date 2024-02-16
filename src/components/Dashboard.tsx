d1fc7ab8d56b35f6938392e5858cecb36ba0b92f
Surafel Araya <surafelay@gmail.com>
2024-02-16 19:09:56 +0300

    Updates

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -35,10 +35,10 @@ const Dashboard = () => {
     transactionID: t.id,
     sender: t.sender.name,
     receiver: t.receiver.name,
-    amount: t.amount,
+    amount: t.amount.toFixed(2),
     currency: t.currency,
     cause: t.cause || "-",
-    createdAt: t.created_at_time,
+    createdAt: new Date(t.created_at_time).toISOString(),
   }));
 
   return (
@@ -51,21 +51,21 @@ const Dashboard = () => {
         ) : (
           <div className="">
             <TanstackTable data={transactions} columns={columns} />
-            <div className="my-10 flxe text-center mr-40">
+            <div className="my-10 flxe text-center mr-40 text-lg">
               <button
                 disabled={page === 1}
-                className="border py-1 px-4 hover:bg-blue-50 disabled:opacity-50"
+                className="border border-slate-300 py-1 px-4 hover:bg-blue-50 disabled:opacity-50 rounded-tl rounded-bl"
                 onClick={() => setPage(page - 1)}
               >
                 Prev
               </button>
 
-              <button className="border-y border-blue-600 py-1 px-4 bg-blue-600 text-white">
+              <button className="border-y border-blue-500 py-1 px-6 bg-blue-500 text-white">
                 {page}
               </button>
               <button
                 disabled={page >= lastPage}
-                className="border py-1 px-4 hover:bg-blue-50 disabled:opacity-50"
+                className="border border-slate-300 py-1 px-4 hover:bg-blue-50 disabled:opacity-50 rounded-tr rounded-br"
                 onClick={() => setPage(page + 1)}
               >
                 Next
--- src/components/common/TanstackTable.tsx
+++ src/components/common/TanstackTable.tsx
@@ -27,7 +27,7 @@ const TanstackTable = ({ data, columns }: Props) => {
               {headerGroup.headers.map((header) => (
                 <th
                   key={header.id}
-                  className="border-t border-b border-slate-100 text-left py-2 px-4 font-medium"
+                  className="border-t border-b border-slate-100 text-left p-3 font-medium"
                 >
                   {header.isPlaceholder
                     ? null
@@ -46,7 +46,7 @@ const TanstackTable = ({ data, columns }: Props) => {
               {row.getVisibleCells().map((cell) => (
                 <td
                   key={cell.id}
-                  className="border-t border-b border-slate-200 py-2 px-4"
+                  className="border-t border-b border-slate-200 p-3"
                 >
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
