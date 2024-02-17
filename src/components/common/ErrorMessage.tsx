99f4693cc724eb22e9cdac2e6fa0fb921092a88d
Surafel Araya <surafelay@gmail.com>
2024-02-17 16:08:03 +0300

    Build ErrorMessage component

---
+++ src/components/common/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const ErrorMessage = () => {
+  return (
+    <div className="flex flex-col bg-gray-50 items-center justify-center lg:mr-40 mt-12 p-6">
+      <h1 className="text-4xl font-bold mb-4">❌ Data retrieval failed.</h1>
+    </div>
+  );
+};
+
+export default ErrorMessage;
