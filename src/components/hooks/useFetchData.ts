b26be871dcbdad16baac91776ed87accf0c56c6d
Surafel Araya <surafelay@gmail.com>
2024-02-17 08:40:04 +0300

    useFetchData: Build a custom hook to fetch data

---
+++ src/components/hooks/useFetchData.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const useFetchData = (
+  key: string,
+  method: string,
+  url: string,
+  body: object
+) => {
+  const fetchData = () => {
+    if (method === "GET") return axios.get(url);
+    if (method === "POST") return axios.post(url, body);
+  };
+
+  return useQuery(key, fetchData, {
+    cacheTime: 3000000, // 5 minutes
+    staleTime: 30000, // 30 seconds
+    refetchOnMount: false,
+    // refetchOnWindowFocus: false,
+  });
+};
+
+export default useFetchData;
