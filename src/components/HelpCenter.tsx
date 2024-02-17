38d0a662397512215736eef3097b7dfc22ae132e
Surafel Araya <surafelay@gmail.com>
2024-02-17 11:20:42 +0300

    HelpCenter: add content

--- src/components/HelpCenter.tsx
+++ src/components/HelpCenter.tsx
@@ -4,10 +4,21 @@ const HelpCenter = () => {
       <div className="row">
         <div className="card">
           <h2>Search</h2>
+          <ul>
+            <li>Search transactions by - </li>
+            <li>Sender account</li>
+            <li>Receiver account</li>
+            <li>Transaction Cause</li>
+          </ul>
         </div>
 
         <div className="card">
           <h2>Sort</h2>
+          <ul>
+            <li>Click on a header to sort</li>
+            <li>Click again to sort in descending order</li>
+            <li>Click again to disable sorting</li>
+          </ul>
         </div>
 
         <div className="card">
