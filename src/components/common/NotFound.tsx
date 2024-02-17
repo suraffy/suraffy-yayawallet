feb7185e9e5ef3acd11f586522f80914f522c461
Surafel Araya <surafelay@gmail.com>
2024-02-17 16:02:14 +0300

    Build a NotFound component

---
+++ src/components/common/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex flex-col bg-gray-50 items-center justify-center lg:mr-40 mt-12 p-6">
+      <h1 className="text-4xl font-bold mb-4">No result matches your search</h1>
+    </div>
+  );
+};
+
+export default NotFound;
