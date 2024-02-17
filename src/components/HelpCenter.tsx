2c6db69594f23fd443cc2b43f94408cdbdaa3dc7
Surafel Araya <surafelay@gmail.com>
2024-02-17 09:37:48 +0300

    Build a HelpCenter component

---
+++ src/components/HelpCenter.tsx
@@ -0,0 +1,21 @@
+const HelpCenter = () => {
+  return (
+    <div className="container my-10">
+      <div className="row">
+        <div className="card">
+          <h2>Search</h2>
+        </div>
+
+        <div className="card">
+          <h2>Sort</h2>
+        </div>
+
+        <div className="card">
+          <h2>Transactions</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HelpCenter;
