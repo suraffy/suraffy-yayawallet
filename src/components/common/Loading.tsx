01f506c753113ab54d7712ae5e0cc4115d602802
Surafel Araya <surafelay@gmail.com>
2024-02-18 12:54:35 +0300

    Remove margin left

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,10 +1,10 @@
 const Loading = () => {
   return (
-    <div className="flex flex-col bg-gray-50 items-center justify-center mt-12 lg:mr-40 p-6">
-      <h1 className="text-4xl font-bold mb-4 flex items-center gap-x-2">
+    <div className="flex flex-col bg-gray-50 items-center justify-center mt-12 p-6">
+      <h1 className="text-4xl font-semibold mb-4 flex items-center gap-x-2">
         <span className="inline-block">Loading </span>
         <span
-          className="inline-block h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
           role="status"
         ></span>
       </h1>
--- src/components/common/NotFound.tsx
+++ src/components/common/NotFound.tsx
@@ -1,7 +1,9 @@
 const NotFound = () => {
   return (
-    <div className="flex flex-col bg-gray-50 items-center justify-center lg:mr-40 mt-12 p-6">
-      <h1 className="text-4xl font-bold mb-4">No result matches your search</h1>
+    <div className="flex flex-col bg-gray-50 items-center justify-center mt-12 p-6">
+      <h1 className="text-3xl font-semibold mb-4">
+        No result matches your search
+      </h1>
     </div>
   );
 };
