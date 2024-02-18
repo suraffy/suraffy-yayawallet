cdb6a58f0e2fb509a52c4b698cb2078800286363
Surafel Araya <surafelay@gmail.com>
2024-02-18 13:00:58 +0300

    Pagination: Fix margin spacing

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -7,21 +7,21 @@ interface Props {
 
 const Pagination = ({ page, lastPage, isFetching, onPageChange }: Props) => {
   return (
-    <div className="my-10 flxe text-center mr-40 text-lg">
+    <div className="my-10 flxe text-center text-lg">
       <button
         disabled={page === 1}
-        className="border border-slate-300 py-1 px-4 hover:bg-blue-50 disabled:opacity-50 rounded-tl rounded-bl"
+        className="inline-block border border-slate-300 py-1 px-4 hover:bg-blue-50 disabled:opacity-50 rounded-tl rounded-bl"
         onClick={() => onPageChange(page - 1)}
       >
         Prev
       </button>
 
-      <button className="border-y border-blue-500 py-1 px-6 bg-blue-500 text-white">
+      <button className="inline-block border-y border-blue-500 py-1 px-6 bg-blue-500 text-white">
         {isFetching ? "..." : page}
       </button>
       <button
         disabled={page >= lastPage}
-        className="border border-slate-300 py-1 px-4 hover:bg-blue-50 disabled:opacity-50 rounded-tr rounded-br"
+        className="inline-block border border-slate-300 py-1 px-4 hover:bg-blue-50 disabled:opacity-50 rounded-tr rounded-br"
         onClick={() => onPageChange(page + 1)}
       >
         Next
