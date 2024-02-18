cfb88960a61ab9f6a1b15852ba706918259cd657
Surafel Araya <surafelay@gmail.com>
2024-02-18 12:59:05 +0300

    Header: hide overflow nav items

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import yayawalletLogo from "../../assets/yayawallet logo.svg";
 
 const Header = () => {
   return (
-    <div className="shadow">
+    <div className="shadow overflow-x-clip">
       <header className="container flex justify-between">
         <img src={yayawalletLogo} width={200} alt="" />
 
@@ -12,12 +12,12 @@ const Header = () => {
               Dashboard
             </a>
           </li>
-          <li className="hover:bg-blue-50 flex items-center">
+          <li className="hover:bg-blue-50 hidden sm:flex items-center">
             <a href="#help-center" className="hover:text-blue-800 p-4">
               Help&nbsp;Center
             </a>
           </li>
-          <li className="hover:bg-blue-50 flex items-center">
+          <li className="hover:bg-blue-50 hidden sm:flex items-center">
             <a href="#" className="hover:text-blue-800 p-4">
               Login
             </a>
